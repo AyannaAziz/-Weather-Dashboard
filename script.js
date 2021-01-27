333c49b55fb42aed3f7c7d647650d7faad9084b0
Ayanna Aziz <ayanna.aziz77@gmail.com>
2021-01-26 23:54:46 -0500

    Page displays denver weather as default upon loading

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 $(document).ready(function(){
-    https://openweathermap
+   //call default city when page loads
+   defaultCity()
+
+   function defaultCity(){
     var url = "https://api.openweathermap.org/data/2.5/weather?q=Atlanta&APPID=0946b5eb988b3caf2e24954f8caf2636"
-    $('#btn-ajax').click(function(){
-        $.get(url,function(data,status){
+           $.get(url,function(data,status){
             console.log(data)
-      })
+            displayData(data)
+            apiFiveDays(data.name)
     })
+
+}
+
+  //function to display difault city 
+   function displayData(data){
+
+    var icon = data.weather[0].icon;
+    var iconUrl = "http://openweathermap.org/img/wn/" + icon + ".png";
+    $('#icon-w').attr('src',iconUrl)
+
+    var currentDate = new Date(data.dt * 1000).toISOString(); 
+    //convert date 
+    var display =  data.name + " (" + moment(currentDate).format("MM/D/YYYY") + ")"
+    $('#cityName').text(display)
+    //convertion to farenheit
+    var tempFar = parseInt((data.main.temp - 273.15)* 9/5 + 32);
+
+    // Display temperature on the page 
+    $('#temperatureSet').text(tempFar+ " °F")
+    // Display humidity on the page 
+   $('#humiditySet').text(data.main.humidity + "%" )
+   // Display  wind 
+   $('#windSet').text(data.wind.speed)
+
+
+
+   }
 })
