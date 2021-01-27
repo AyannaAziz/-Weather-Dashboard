2dde08bc39a7fce702145630a49040b75cee01d5
Ayanna Aziz <ayanna.aziz77@gmail.com>
2021-01-26 23:54:59 -0500

    Page displays denver weather as default upon loading

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
    defaultCity()
 
    function defaultCity(){
-    var url = "https://api.openweathermap.org/data/2.5/weather?q=Atlanta&APPID=0946b5eb988b3caf2e24954f8caf2636"
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=Denver&APPID=0946b5eb988b3caf2e24954f8caf2636"
            $.get(url,function(data,status){
             console.log(data)
             displayData(data)
@@ -12,7 +12,7 @@ $(document).ready(function(){
 
 }
 
-  //function to display difault city 
+  //function to display default city 
    function displayData(data){
 
     var icon = data.weather[0].icon;
@@ -20,9 +20,11 @@ $(document).ready(function(){
     $('#icon-w').attr('src',iconUrl)
 
     var currentDate = new Date(data.dt * 1000).toISOString(); 
+    
     //convert date 
     var display =  data.name + " (" + moment(currentDate).format("MM/D/YYYY") + ")"
     $('#cityName').text(display)
+    
     //convertion to farenheit
     var tempFar = parseInt((data.main.temp - 273.15)* 9/5 + 32);
 
